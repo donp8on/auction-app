9fae6fd074edeeeb9fe192cda6e5d9eb5457cd9f
donp8on <paytonadonis@gmail.com>
2025-04-23 19:55:09 -0400

    added end auction button

--- app.py
+++ app.py
@@ -94,4 +94,5 @@ def start_auction():
         thread.start()
 
 if __name__ == '__main__':
-    socketio.run(app, host='localhost', debug=True, port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)
+
